cb83ff21840b5eaab71328d97742146224b39f4f
Yassin <inyasssin@gmail.com>
2021-06-22 00:01:12 -0400

    Work section not done

---
+++ main.js
@@ -0,0 +1,60 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+var firebaseConfig = {
+    apiKey: "AIzaSyDSP_7_00oO2-dzma9wOQhBETiCjDUIjTo",
+    authDomain: "contactform-portfolio-3514d.firebaseapp.com",
+    projectId: "contactform-portfolio-3514d",
+    storageBucket: "contactform-portfolio-3514d.appspot.com",
+    messagingSenderId: "773954069985",
+    appId: "1:773954069985:web:a2744ca23792fd6d685ea0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('messages');
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e){
+    e.preventDefault();
+  
+    // Get values
+    var name = getInputVal('name');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+  
+    // Save message
+    saveMessage(name, company, email, phone, message);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, company, email, phone, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      company:company,
+      email:email,
+      phone:phone,
+      message:message
+    });
+  }
